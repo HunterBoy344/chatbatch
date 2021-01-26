e3fc70ae451f67e02e1cfbee3658c4904d9cbb7d
HunterBoy344 <66140471+HunterBoy344@users.noreply.github.com>
2021-01-26 16:45:08 -0500

    Add files via upload

---
+++ looping-realtime-drive.bat
@@ -0,0 +1,15 @@
+@echo off
+set /p "driveletter=Enter Google Drive drive letter: "
+set /p "chatname=Enter chat folder path in quotations: "
+for /f %%t in ('cd') do set currentlocation=%%t
+MODE 60,20
+cd /D %driveletter%:\%chatname%
+:loop
+for /f %%e in (%currentlocation%\exit.txt) do set exitcounter=%%e
+if "%exitcounter%"=="exit" (
+exit
+)
+cls
+type chat.txt
+timeout 1 >nul
+goto loop
